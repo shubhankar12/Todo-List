37b9bef4fcb54444acc31d8d0240cd08019b5c30
Shubhankar Suman <67308106+shubhankar12@users.noreply.github.com>
2022-11-04 14:10:11 +0530

    Add files via upload

---
+++ precache-manifest.d41e98bc0da23f4f61c606ef18d81137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c88a80783aa0c4254d15",
+    "url": "/static/css/main.ac4cc292.chunk.css"
+  },
+  {
+    "revision": "c88a80783aa0c4254d15",
+    "url": "/static/js/main.c88a8078.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "2a9ac7b02052de5a797f",
+    "url": "/static/js/2.2a9ac7b0.chunk.js"
+  },
+  {
+    "revision": "4431f52a260a7d5c73e4",
+    "url": "/static/js/runtime~main.4431f52a.js"
+  },
+  {
+    "revision": "e317cdd7f5937a7ad3700f6daafec0d5",
+    "url": "/index.html"
+  }
+];
